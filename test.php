ef010767d0cca753c3cedca4e40ff7812a87fe2c
estrakio <walterkarl@hotmail.fr>
2022-11-22 21:48:35 +0100

    Ne pas supprimer test

--- test.php
+++ test.php
@@ -1,24 +1,154 @@
 <?php
-    // Page de test pour comprendre le format de données pour l'envoi via la fonction SQL 
-    $_GET['plaque_d_immatriculation'] = "boumboum";
 
-    $condition = "'".$_GET['plaque_d_immatriculation']."'";
-    $sql = "SELECT
-            *
-            FROM vehicule
-            LEFT JOIN modele
-            ON  vehicule.id_1 = modele.id
-            LEFT JOIN location
-            ON  vehicule.id = location.id
-            LEFT JOIN clients
-            ON  Location.id_1 = clients.id
-            WHERE vehicule.plaque_d_immatriculation = ".$condition.";";
 
-    $conn = pg_connect("host=db dbname=vroooom user=vroooom password=vroooom");
-    
-    $result = pg_fetch_all(pg_query($conn, $sql));
+// Créer des fiches imprimable de chaque table;
 
-    echo("<pre>");
-    var_dump($result);
-    echo("</pre>");
+//echo (" <pre>");
+//var_dump($_POST);
+//echo ("</pre>");
+
+if (isset($_POST["choix"])) {
+
+    function test($tableData)
+    {
+        echo ("<pre>");
+        var_dump($tableData);
+        echo ("</pre>");
+        foreach ($tableData  as $ligne) {
+            foreach ($ligne  as $champ => $value) {
+                //echo($champ);
+                echo ("<tr>");
+                echo ("<td>" . $value[$champ] . "</td>");
+                echo ("</tr>");
+            } 
+        } 
+    }
+
+    function recupTable($table)
+    {
+        $tabData = tableSqlOrderById($table,'nom, prenom');
+        return ($tabData);
+    }
+
+    function affichTable($tableData, $titre)
+    {
+
 ?>
+
+        <div class="row text-center  h3">
+            <b style="margin-top:5vh;"><?php $titre ?></b>
+        </div>
+        <div class="row">
+            <div>
+                <table class="table table-striped table-dark h5">
+
+                    <thead class="thead-dark">
+                        <tr>
+                            <?php
+                            foreach ($tableData  as $ligne) {
+                                if(next($tableData)){
+                                }else{
+                                    foreach ($ligne  as $champ => $value) {
+                                        //echo($champ);
+                                        echo ("<th scope='col'>".$champ."</th>");
+                                    }
+                                }    
+
+                            } ?>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                <?php
+
+
+                echo ('<div class="row">');
+                foreach ($tableData  as $ligne) {
+                    echo ("<tr>");
+                    foreach ($ligne  as $champ => $value) {
+                        //echo($champ);
+                        echo ("<td>" . $value . "</td>");
+                    } 
+                    echo ("</tr>");
+                } 
+                echo ('</tbody>');
+                echo ('</table>');
+                echo ('</div>');
+                echo ('</div>');
+            }
+
+            switch ($_POST["choix"]) {
+
+                case 'clients':
+                    # code...
+                    $table = "clients";
+                    $titre = "clients";
+                    $tableData =  recupTable($table);
+                    //test($tableData);
+                    affichTable($tableData, $titre);
+                    break;
+
+                case 'garage':
+                    # code...
+                    $table = "garage";
+                    $titre = 'garage';
+                    $tableData =  recupTable($table);
+                    affichTable($tableData, $titre);
+                    break;
+
+                case 'societe Expert':
+                    # code...
+                    $table = "societeexpert";
+                    $titre = 'societe Expert';
+                    $tableData =  recupTable($table);
+                    affichTable($tableData, $titre);
+                    break;
+
+                case 'expert':
+                    # code...
+                    $table = "expert";
+                    $titre = "expert";
+                    $tableData =  recupTable($table);
+                    affichTable($tableData, $titre);
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
+        }else{
+                ?>
+                <div class="row">
+                    <div class="col-3 "></div>
+                    <div class="col-6">
+                        <form action="/formulaire.php?content=test" method="post" id="formChoix">
+                            <input hidden value="choix" name="content" id="content">
+                            <br>
+                            <div>
+                                <label for="choix">Sélectionnez la table à extraire : </label>
+                                <select class="form-control" id="" name="choix">
+                                    <option disabled selected value>-- Menu déroulant -- </option>
+                                    <option>clients</option>
+                                    <option>garage</option>
+                                    <option>societe Expert</option>
+                                    <option>expert</option>
+                                </select>
+                            </div>
+                            <br>
+                        </form>
+                        <div class="text-center">
+                            <button class="btn btn-secondary btn-lg" type="submit" form="formChoix" value="Submit">Envoyer</button>
+                            <br><br>
+                        </div>
+                    </div>
+                    <div class="col-3">
+
+                    </div>
+                </div>
+
+                <?php
+
+        }
+
+                ?>
