b5a021451cbd6c1dbe85020917803baa1935c30f
estrakio <walterkarl@hotmail.fr>
2023-04-11 23:36:03 +0200

    modification accueil

--- accueil.php
+++ accueil.php
@@ -1,28 +1,35 @@
-<body >
-    
-    <div class="row"  id="content">
-    
-            <div class="col-2"></div>
-            <div class="col-4">
-              <br>
-              <br>
-              <h1><b class="term">UNE PHRASE COURTE</b></h1>
-              <h3>LES APPLICATIONS WEB ET MOBILE SONT EN COUR DE REALISATION</h3>
-              <hr><hr>
-              <p>LE RESTE DU SITE ARRIVE BIENTOT
-                 LE LOGO A DROITE N'EST PAS 
-                 DEFINITIF MAIS IL EST TRES ELEGANT. 
+<div class="row" id="content">
 
-              </p>
-              <br>
-              ALLONS NOUS LE GARDER???<br>
-              <hr>
-              LA REPONSE ARRIVE BIENTOT...
-            </div>
-            <div class="col-4">
-              <img id="img" src="./images/noir.png">
-            </div>
-            <div class="col-2"></div>
-    </div>       
-  
-</html>
+  <div class="col-2"></div>
+  <div class="col-4">
+    <br>
+    <br>
+    <h1><b class="term">Site de gestion de l'entreprise de leasing Vroooom</b></h1>
+    <h3>Ce projet contient un site web ainsi qu'une application mobile</h3>
+    <hr>
+    <hr>
+    <div class="row">
+      <div class="col-6">
+        <h4>
+          <b class="term">Site web</b>
+        </h4>
+        le site sert de gestionnaire pour la société de leasing.
+        <br>
+        c'est ici que toute les données de base sont créer, modifiées et supprimées.
+      </div>
+      <div class="col-6">
+        <h4>
+          <b class="term">Application mobile</b>
+        </h4>l'application mobile sert a remplir les formulaires des experts qui vont vérifier l'état de la voiture,
+        <br>
+        il y remplisse tout les dégats que la voiture à subi durant ça location.
+      </div>
+    </div>
+    <br>
+    <hr>
+  </div>
+  <div class="col-4 text-center">
+    <img id="img" src="./images/noir.png">
+  </div>
+  <div class="col-2"></div>
+</div>
--- index.php
+++ index.php
@@ -1,90 +1,102 @@
 <?php
-
 include 'connexion.php';
-
 ?>
 
 <!DOCTYPE HTML>
 <html>
-    <head>
-        <title> Site de TEST </title>
 
-        <!-- css necessaire pour Bootstrap -->
-        <link rel="stylesheet" href="./bootstrap/5.1.2/css/bootstrap.min.css">
-        
-        <!--  Mon css perso pour inclure les polices de caracteres-->
-        <link rel="stylesheet" href="./css/fonts.css">
-        <link rel="stylesheet" href="./css/index.css">
+<head>
+  <title> Gestion Vroooom </title>
 
-        <!-- scripts necessaires pour bootstrap -->
-        <script src="./bootstrap/5.1.2/js/bootstrap.bundle.min.js"></script>
+  <!-- css necessaire pour Bootstrap -->
+  <link rel="stylesheet" href="./bootstrap/5.1.2/css/bootstrap.min.css">
 
-        <!-- Jquery needed -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="./js/scripts.js"></script>
-        <script src="./tinymce/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+  <!--  Mon css perso pour inclure les polices de caracteres-->
+  <link rel="stylesheet" href="./css/fonts.css">
+  <link rel="stylesheet" href="./css/index.css">
 
-    <!-- Function used to shrink nav bar removing paddings and adding black background -->
-        <script>
-            $(window).scroll(function() {
-                if ($(document).scrollTop() > 75) {
-                    $('.nav').addClass('affix');
-                    console.log("OK");
-                } else {
-                    $('.nav').removeClass('affix');
-                }
-            });
-        </script>
+  <!-- scripts necessaires pour bootstrap -->
+  <script src="./bootstrap/5.1.2/js/bootstrap.bundle.min.js"></script>
 
-    </head> 
+  <!-- Jquery needed -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="./js/scripts.js"></script>
+  <script src="./tinymce/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
 
-    <body>
-        <!-- DIV = Ce que je veux dépendant de ce que je met dedans  -->
-        <nav class="nav">
-            <div class="container">
-                <div class="logo">
-                    <a class="titre" id="accueil" href="/index.php?content=accueil">Accueil</a>
-                </div>
-                <div id="mainListDiv" class="main_list">
-                    <ul class="navlinks">
-                        <li><a href="/formulaire.php?content=liste">Liste</a></li>
-                        <li><a href="/formulaire.php?content=clients">Clients</a></li>
-                        <li><a href="/formulaire.php?content=garage">Garage</a></li>
-                        <li><a href="/formulaire.php?content=expert">Expert</a></li>
-                        <li><a href="/formulaire.php?content=location">Location</a></li>
-                        <li><a href="/formulaire.php?content=rendezVous">Rendez-Vous</a></li>
-                        <li><a href="/formulaire.php?content=societeExperts">Société expert</a></li>
-                        <li><a href="/formulaire.php?content=vehicule">vehicule</a></li>
-                    </ul>
-                </div>
-                <span class="navTrigger">
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                </span>
-            </div>
-        </nav>
-        <section class="home">
-        </section>
+  <script>
+    $(window).scroll(function () {
+      if ($(document).scrollTop() > 75) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+      } else {
+        $('.nav').removeClass('affix');
+      }
+    });
+  </script>
+
+</head>
+
+<body>
+  <nav class="nav">
+    <div class="container">
+      <div class="logo">
+        <a class="titre" id="accueil" href="/index.php?content=accueil">Accueil</a>
+      </div>
+      <div id="mainListDiv" class="main_list">
+        <ul class="navlinks">
+          <li>
+            <a href="/formulaire.php?content=liste">Liste</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=clients">Clients</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=garage">Garage</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=expert">Expert</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=location">Location</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=rendezVous">Rendez-Vous</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=societeExperts">Société expert</a>
+          </li>
+          <li>
+            <a href="/formulaire.php?content=vehicule">vehicule</a>
+          </li>
+        </ul>
+      </div>
+      <span class="navTrigger">
+        <i></i>
+        <i></i>
+        <i></i>
+      </span>
+    </div>
+  </nav>
+  <section class="home">
+  </section>
 
 
-        <div id="content">
-            <?php
-                if (isset($_GET["content"])) {
+  <div id="content">
+    <?php
+    if (isset($_GET["content"])) {
 
-                    include $_GET["content"].".php";
-                }
-                else{
-                    include "accueil.php";
-                }
-            ?>
+      include $_GET["content"] . ".php";
+    } else {
+      include "accueil.php";
+    }
+    ?>
 
-        </div>
-        
-        <div id="footer" class="row">
-            <?php include "footer.php"; ?>
-        </div>
+  </div>
 
-    </body>
+  <div id="footer" class="row">
+    <?php include "footer.php"; ?>
+  </div>
+
+</body>
 
 </html>
