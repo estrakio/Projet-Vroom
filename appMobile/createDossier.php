bf9b40dd6bc4eca802548832b636f8dfa1a39dea
estrakio <walterkarl@hotmail.fr>
2023-06-11 23:13:17 +0200

    fix asso_19

--- appMobile/createDossier.php
+++ appMobile/createDossier.php
@@ -1,6 +1,8 @@
 
 <?php
 include_once("../connexion.php");
+
+// Fonction Vérifiant si la plaque envoyé par l'application existe déjà dans la base de donnée
 function testPlaqueDossier($plaque){
     $table = tableSql("Dossier");
     $already_created = false;
@@ -13,13 +15,15 @@ function testPlaqueDossier($plaque){
     }
     return $already_created;
 }
+
+// Fonction créant un dossier pour la plaque envoyé par l'application
 function create_dossier($plaque){
 
     $conn = pg_connect("host=db dbname=vroooom user=vroooom password=vroooom");
     $plaque = pg_escape_string($conn, $plaque);
     $sql = "INSERT INTO dossier (plaque_d_immatriculation) VALUES ('$plaque') RETURNING id;";
     
-    $result = pg_fetch_assoc(pg_query($conn, $sql));
+    $result = pg_fetch_assoc(pg_query($conn, $sql));  
 
     $id_dossier = $result["id"];
 
@@ -37,8 +41,8 @@ function create_dossier($plaque){
         // echo $img;
         // echo base64_encode($img);
 
-        $id_expertise = pg_fetch_assoc(pg_query($conn, "INSERT INTO piece (piece, description, lienphoto) VALUES ('".$expertise['piece']."', '".$expertise['description']."', '$img') RETURNING id;"))['id'];
-        pg_query($conn, "INSERT INTO asso19 (id, id_1) VALUES ('$id_expertise', '$id_dossier');");
+        $id_expertise = pg_fetch_assoc(pg_query($conn, "INSERT INTO piece (piece, description, lienphoto, id_1) VALUES ('".$expertise['piece']."', '".$expertise['description']."', '$img', $id_dossier);"));
+        //pg_query($conn, "INSERT INTO asso19 (id, id_1) VALUES ('$id_expertise', '$id_dossier');");
 
         // insertSql('asso19', array('id' => $id_expertise, 'id_1' => $id_dossier));
     }
@@ -64,6 +68,7 @@ function create_dossier($plaque){
     // return $tableau;
 }
 
+// fonction permettant de récupérer un dossier pour l'app mobile
 function get_dossier($plaque) {
     $conn = pg_connect("host=db dbname=vroooom user=vroooom password=vroooom");
     $plaque = pg_escape_string($conn, $plaque);
@@ -71,10 +76,8 @@ function get_dossier($plaque) {
     $sql = "SELECT
             *
             FROM dossier
-            LEFT JOIN asso19
-            ON  dossier.id = asso19.id_1
             LEFT JOIN piece
-            ON asso19.id = piece.id
+            ON  dossier.id = piece.id_1
             WHERE dossier.plaque_d_immatriculation = ".$condition.";";
     
     $result = pg_fetch_all(pg_query($conn, $sql));
@@ -116,8 +119,9 @@ function updateData($plaque){
     foreach ($dossier as $i => $expertise) {
         // var_dump($expertise);
 
-        $id_expertise = pg_fetch_assoc(pg_query($conn, "INSERT INTO piece (piece, description, lienphoto) VALUES ('".$expertise['piece']."', '".$expertise['description']."', '".$expertise['lienphoto']."') RETURNING id;"))['id'];
-        pg_query($conn, "INSERT INTO asso19 (id, id_1) VALUES ('$id_expertise', '$id_dossier');");
+
+        $id_expertise = pg_fetch_assoc(pg_query($conn, "INSERT INTO piece (piece, description, lienphoto, id_1) VALUES ('".$expertise['piece']."', '".$expertise['description']."', '".$expertise['lienphoto']."', $id_dossier);"));
+
     }
 }
 
@@ -125,8 +129,11 @@ function updateData($plaque){
 if (isset($_POST["plaque"])){
     $already_created = testPlaqueDossier($_POST["plaque"]);
 
+    // Si la plaque envoyé par l'app n'existe pas dans la base de donnée
     if(!$already_created){
+        // Si l'app envoie un dossier
         if (isset($_POST["dossier"])){
+            
             create_dossier($_POST["plaque"]);
             echo(json_encode(array( "state" => "success")));
         }else{
